02f60435610849729068ee0d05e16367bf2bbcc2
tkorkotashvili <temo.korkotashvili@gmail.com>
2020-10-14 20:53:53 +0400

    Content Layout

--- Books/src/app/app.module.ts
+++ Books/src/app/app.module.ts
@@ -4,16 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
-import { ContentHeaderComponent } from './layout/content-layout/content-header/content-header.component';
-import { ContentFooterComponent } from './layout/content-layout/content-footer/content-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContentLayoutComponent,
-    ContentHeaderComponent,
-    ContentFooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
