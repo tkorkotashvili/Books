ae7326d1de486d2e9bd0f2ba16271d48161a64ed
Temo <temo.korkotashvili@gmail.com>
2020-10-16 11:43:50 +0400

    Content Layout

--- Books/src/app/layout/content-layout/content-layout.component.ts
+++ Books/src/app/layout/content-layout/content-layout.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ContentHeaderComponent } from './content-header/content-header.component';
+import { ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 @Component({
   selector: 'app-content-layout',
   templateUrl: './content-layout.component.html',
   styleUrls: ['./content-layout.component.scss']
 })
+
 export class ContentLayoutComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
+  }
+  collapsedNav: boolean;
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
   }
 
 }
